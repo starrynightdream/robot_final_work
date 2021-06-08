d6d15343f2e5f317ef298f024e26eeec1e65e6da
whl <1023680139@qq.com>
2021-06-08 15:04:06 +0800

    feat: 添加获取数据

--- nodeServe/app.js
+++ nodeServe/app.js
@@ -2,6 +2,8 @@ const path = require('path');
 const cp = require('child_process');
 const express = require('express');
 
+const mutil = require('./util');
+
 const port = 3000;
 const main1FilePath = '../main.py';
 const main2FilePath = '../test.py';
@@ -12,6 +14,19 @@ const ejs = require('ejs')
 
 const app = express();
 const threadObj = {};
+const pythonFiles = {};
+
+
+const init = async () =>{
+    let files = await mutil.findAllFileLike('../', ['py']);
+    for (let file of files){
+        let fileN = file.split('\\');
+        fileN = fileN[ fileN.length - 1];
+        fileN = fileN.split('.')[0];
+        pythonFiles[fileN] = file;
+    }
+}
+init();
 
 app.set('views', path.join(__dirname, '/view')); 
 app.engine('html', ejs.renderFile);
@@ -26,19 +41,21 @@ app.use('/', express.static( path.join(__dirname, '/public/')))
 
 app.post('/runPy', (req, res)=>{
     let key = req.body.pyFile.trim();
-    let file = '';
-    switch(key)
-    {
-        case 'main1':
-            file = main1FilePath;
-            break;
-        case 'main2':
-            file = main2FilePath;
-            break;
-        case 'stop':
-            file = stopFilePath;
-            break;
-    }
+    console.log(key)
+    let file = pythonFiles[key];
+
+    // switch(key)
+    // {
+    //     case 'main1':
+    //         file = main1FilePath;
+    //         break;
+    //     case 'main2':
+    //         file = main2FilePath;
+    //         break;
+    //     case 'stop':
+    //         file = stopFilePath;
+    //         break;
+    // }
     if (!file){
         res.json({
             stdout: 'no such file'
@@ -73,20 +90,20 @@ app.post('/runPy', (req, res)=>{
 
 app.post('/kill', (req, res) =>{
     let key = req.body.pyFile.trim();
-    let file = '';
-    switch(key)
-    {
-        case 'main1':
-            file = main1FilePath;
-            break;
-        case 'main2':
-            file = main2FilePath;
-            break;
-        case 'stop':
-            file = stopFilePath;
-            break;
+    let file = pythonFiles[key];
+    // switch(key)
+    // {
+    //     case 'main1':
+    //         file = main1FilePath;
+    //         break;
+    //     case 'main2':
+    //         file = main2FilePath;
+    //         break;
+    //     case 'stop':
+    //         file = stopFilePath;
+    //         break;
 
-    }
+    // }
     if (!(file && threadObj[key])){
         res.json({
             stdout: 'no such thread'
@@ -102,9 +119,10 @@ app.post('/kill', (req, res) =>{
     });
 });
 
-app.use('/', (req, res)=>{
+app.use('/', async (req, res)=>{
     // 需要对文件夹内的python脚本做统计，而后再返回渲染的页面
-    res.render('index', {files: ['main1', 'main2', 'stop']});
+    // res.render('index', {files: ['main1', 'main2', 'stop']});
+    res.render('index', {files: Object.keys(pythonFiles)});
 });
 
 app.listen(port, ()=>{
